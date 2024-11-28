4aee9e68a185219e2754afa8b800fab2e172c2c7
TGEC1 <traditionalgamez1@gmail.com>
2024-11-28 08:27:41 +0800

    added constructor

---
+++ games.js
@@ -0,0 +1,10 @@
+class Game {
+    constructor(game_id, game_name, game_description, game_image, game_link) {
+        this.game_id = game_id;
+        this.game_name = game_name;
+        this.game_description = game_description;
+        this.game_image = game_image;
+        this.game_link = game_link;
+    }
+
+}
