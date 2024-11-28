490c50da670ce54dd93d50fc93fafdacb99bc3e3
TGEC1 <traditionalgamez1@gmail.com>
2024-11-28 10:39:18 +0800

    added const games

--- games.js
+++ games.js
@@ -56,10 +56,9 @@ class OutdoorGame extends Game {
 class StreetGame extends Game {
     constructor(game_id, game_name, game_description, game_image, game_link, team_size) {
         super(game_id, game_name, game_description, game_image, game_link);
-        this.team_size = team_size; // Additional property for street games
+        this.team_size = team_size; 
     }
 
-    // Overriding renderCard for StreetGame (Polymorphism)
     renderCard() {
         const details = this.getGameDetails();
         return `
@@ -75,3 +74,14 @@ class StreetGame extends Game {
     }
 }
 
+const games = [
+    new OutdoorGame(1, "Tumbang Preso", "A classic Filipino outdoor game where players try to knock down a can with a slipper.", "./pictures/tumbang preso.jpg", "./tumbangpreso.html", "Medium"),
+    new StreetGame(2, "Taguan", "The Filipino version of hide and seek where players search for hidden individuals.", "./pictures/taguan.jpg", "./taguan.html", 2),
+    new OutdoorGame(3, "Sipa", "A traditional Filipino game involving a small ball made of rubber or rags that is kicked using the feet.", "./pictures/sipa.webp", "./sipa.html", "Hard"),
+    new StreetGame(4, "Piko", "A Filipino hopscotch game where players hop from square to square to reach the top without touching the lines.", "./pictures/piko.jpg", "./piko.html", 4),
+    new OutdoorGame(5, "Patintero", "A Filipino street game where two teams try to pass through lines drawn on the ground while avoiding being tagged.", "./pictures/patentiro.jpg", "./patintero.html", "Medium"),
+    new StreetGame(6, "Luksong Tinik", "A Filipino jumping game where one player jumps over the others' legs.", "./pictures/Luksong-Tinik.webp", "./luksongtinik.html", 3),
+    new OutdoorGame(7, "Luksong Baka", "A Filipino game involving jumping over a person who is bending down, and the height increases as the game goes on.", "./pictures/Luksong Baka.jpg", "./luksongbeka.html", "Hard"),
+    new OutdoorGame(8, "Kadang-Kadang", "A traditional Filipino game that involves balancing on bamboo stilts while walking.", "./", "./kadangkadang.html", "Easy"),
+    new StreetGame(9, "Agawan Base", "A Filipino team game where players try to take over the opponent's base without being tagged.", "./pictures/agawan_base.jpg", "./agawanbase.html", 4)
+];
