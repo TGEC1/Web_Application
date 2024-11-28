c551ab1c3786a97798615c7f647deeb3c854fe24
TGEC1 <traditionalgamez1@gmail.com>
2024-11-28 08:30:43 +0800

    added OutdoorGame extends Game

--- games.js
+++ games.js
@@ -31,3 +31,24 @@ class Game {
     }
 
 }
+
+class OutdoorGame extends Game {
+    constructor(game_id, game_name, game_description, game_image, game_link, difficulty_level) {
+        super(game_id, game_name, game_description, game_image, game_link);
+        this.difficulty_level = difficulty_level; 
+    }
+
+    renderCard() {
+        const details = this.getGameDetails();
+        return `
+            <div class="card">
+                <img src="${details.image}" class="card-img-top" alt="${details.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${details.name} - Difficulty: ${this.difficulty_level}</h5>
+                    <p class="card-text">${details.description}</p>
+                    <a href="${details.link}" class="btn btn-primary">Click</a>
+                </div>
+            </div>
+        `;
+    }
+}
